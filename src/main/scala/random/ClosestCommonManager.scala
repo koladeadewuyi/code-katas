b6fbbf1c696d72693939d0d8c72149ea5c486322
Kolade <koladeadewuyi@gmail.com>
2018-06-10 22:11:32 +0100

    Update closestCommonManager, employee contains subordinates

--- src/main/scala/random/ClosestCommonManager.scala
+++ src/main/scala/random/ClosestCommonManager.scala
@@ -2,33 +2,34 @@ package random
 
 object ClosestCommonManager {
 
-  val bill = Employee(1, "Bill", Nil)
+  val milton = Employee(8, "Milton", Nil)
+  val nina = Employee(9, "Nina", Nil)
 
-  val dom = Employee(2, "Dom", Seq(bill))
-  val samir = Employee(3, "Samir", Seq(bill))
-  val michael = Employee(4, "Michael", Seq(bill))
+  val bob = Employee(6, "Bob", Nil)
+  val porter = Employee(7, "Porter", Nil)
+  val peter = Employee(5, "Peter", Seq(milton, nina))
 
-  val peter = Employee(5, "Peter", Seq(dom))
-  val bob = Employee(6, "Bob", Seq(dom))
-  val porter = Employee(7, "Porter", Seq(dom))
+  val dom = Employee(2, "Dom", Seq(peter, bob, porter))
+  val samir = Employee(3, "Samir", Nil)
+  val michael = Employee(4, "Michael", Nil)
 
-  val milton = Employee(8, "Milton", Seq(peter))
-  val nina = Employee(9, "Nina", Seq(peter))
+  val bill = Employee(1, "Bill", Seq(dom, samir, michael))
 
   def getClosestCommonManager(employees: Seq[Employee]): Employee = {
-    val managers = employees.map(employee => getManagers(employee, employee.reportsTo))
-    val commonManagers = managers.flatten.groupBy(_.id)
-    commonManagers(commonManagers.keySet.min).head
+    val employeesManagers = employees.map(employee => getManagers(bill, employee, Seq(bill)).toSet)
+    val commonManagers = employeesManagers.reduce(_ intersect _)
+    commonManagers.toSeq.maxBy(_.id)
   }
 
-  private def getManagers(employee: Employee, managers: Seq[Employee]): Seq[Employee] = {
-    if (employee.reportsTo.isEmpty) managers
+  private def getManagers(manager: Employee, employee: Employee, managers: Seq[Employee]): Seq[Employee] = {
+    val subordinates = manager.subordinates
+    if (subordinates.isEmpty) managers
     else {
-      val lineManagers = employee.reportsTo
-      lineManagers.flatMap(employee => getManagers(employee, managers))
+      if (subordinates.contains(employee)) managers :+ manager
+      else subordinates.flatMap(subordinate => getManagers(subordinate, employee, managers :+ manager))
     }
   }
 
 }
 
-case class Employee(id: Int, name: String, reportsTo: Seq[Employee])
+case class Employee(id: Int, name: String, subordinates: Seq[Employee])
