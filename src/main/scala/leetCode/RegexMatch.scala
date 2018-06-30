698d9f9d96bb8345566d5a074821ac70649c8df5
Kolade <koladeadewuyi@gmail.com>
2018-06-30 11:58:36 +0100

    Add complete solution to leetCode (RegexMatch)

--- src/main/scala/leetCode/RegexMatch.scala
+++ src/main/scala/leetCode/RegexMatch.scala
@@ -14,18 +14,18 @@ object RegexMatch {
   @tailrec
   private def matchRecurse(s: String, p: String, prevChar: Char, prevRegexChar: Char, matches: Boolean = true): Boolean = {
     if (s.nonEmpty && p.isEmpty) false
-    else if (s.isEmpty && p.isEmpty) matches
-    else if (s.isEmpty && p.nonEmpty) false
+    else if (s.isEmpty || p.isEmpty) matches
     else {
       prevRegexChar match {
         case MatchAnyChar =>
           matchRecurse(s.tail, p.tail, s.head, p.head)
         case MatchZeroOrMoreTimes =>
-          if (s.head == prevChar) matchRecurse(s.tail, p, s.head, prevRegexChar)
-          else if (s.head != prevChar) matchRecurse(s.tail, p.tail, s.head, p.head)
-          else false
+          if (s.head == prevChar) matchRecurse(s.tail, p.tail, s.head, p.head)
+          else if (p.head != MatchZeroOrMoreTimes) matchRecurse(s, p, s.head, p.head)
+          else matchRecurse(s, p.tail, prevChar, p.head)
         case anyChar =>
           if (prevChar == anyChar) matchRecurse(s.tail, p.tail, s.head, p.head)
+          else if (p.head == MatchZeroOrMoreTimes) matchRecurse(s, p.tail.tail, prevChar, p.tail.head)
           else false
       }
     }
